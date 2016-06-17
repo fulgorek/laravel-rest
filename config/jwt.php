38cdc8a1ebc92a10449a03d3b7bfaebff03ade43
Alejandro Torres <info@alejandrotorres.xyz>
2016-06-17 18:13:52 -0500

    jwt secret

--- config/jwt.php
+++ config/jwt.php
@@ -21,7 +21,7 @@ return [
     |
     */
 
-    'secret' => env('JWT_SECRET', 'changeme'),
+    'secret' => env('JWT_SECRET', '4ghyeKCZiv17aqjsjBCLBhOiQpZx7u5T'),
 
     /*
     |--------------------------------------------------------------------------
