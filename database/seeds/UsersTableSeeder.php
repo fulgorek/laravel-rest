32dd86aa990efac4a729f91a64da5ee0d0beb209
Alejandro Torres <info@alejandrotorres.xyz>
2016-06-17 18:22:58 -0500

    users seed

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name'       => 'admin',
+            'email'      => 'admin@adgate.com',
+            'password'   => bcrypt('admin'),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+    }
+}
